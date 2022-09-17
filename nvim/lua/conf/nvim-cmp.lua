3bd7a1588897e9491ef0a0a5f61932c405c14a98
xundaoxd <xundaoxd@outlook.com>
2022-09-17 10:34:00 +0800

    improve luasnip

--- nvim/lua/conf/nvim-cmp.lua
+++ nvim/lua/conf/nvim-cmp.lua
@@ -9,13 +9,43 @@ cmp.setup({
         end
     },
     mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
         ['<CR>'] = cmp.mapping.confirm({
             select = true ,
             behavior = cmp.ConfirmBehavior.Replace
         })
     }),
+    formatting = {
+        format = function(entry, item)
+            item.menu = ({
+                nvim_lsp = '[LSP]',
+                luasnip = '[Snippet]',
+                buffer = '[Buffer]',
+                path = '[Path]',
+                emoji = '[Emoji]',
+            })[entry.source.name]
+            return item
+        end
+    },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
