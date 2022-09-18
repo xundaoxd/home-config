ed5473bda8c11bec8728de1ea8cd16b97889222f
xundaoxd <xundaoxd@outlook.com>
2022-09-18 17:54:28 +0800

    new telescope theme for find_files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 require('telescope').setup()
 
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files(require('telescope.themes').get_dropdown()) end)
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
