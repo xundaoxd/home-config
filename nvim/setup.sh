2822d317853e353541b98d0122f25172882ca24c
xundaoxd <xundaoxd@outlook.com>
2022-09-19 22:42:07 +0800

    refactor setup

--- nvim/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pacman -S --noconfirm neovim python-pynvim xclip python-pip ripgrep-all cmake
-
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# common
+pacman -S --noconfirm python-pip ripgrep-all cmake npm
+
+# nvim
+pacman -S --noconfirm neovim python-pynvim xclip
+
