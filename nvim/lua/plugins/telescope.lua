77cd9696bfb478923436378fd1e6235ac27802f0
yinchaogao <yinchaogao@deeproute.ai>
2022-09-20 11:26:25 +0800

    update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 require('telescope').setup()
 
-vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files(require('telescope.themes').get_dropdown()) end)
-vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown()) end)
-vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers(require('telescope.themes').get_dropdown()) end)
-vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown()) end)
+local dropdown = require('telescope.themes').get_dropdown()
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files(dropdown) end)
+vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep(dropdown) end)
+vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers(dropdown) end)
+vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags(dropdown) end)
 
