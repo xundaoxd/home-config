332dbc9646c4599d283c7e571891c6f0d27fcb55
xundaoxd <xundaoxd@outlook.com>
2022-09-21 22:52:12 +0800

    default theme for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,7 @@
 require('telescope').setup()
 
-local dropdown = require('telescope.themes').get_dropdown()
-vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files(dropdown) end)
-vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep(dropdown) end)
-vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers(dropdown) end)
-vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags(dropdown) end)
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end)
+vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end)
+vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end)
+vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end)
 
