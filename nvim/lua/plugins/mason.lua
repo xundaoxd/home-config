821c05825ee949d0363393e652ef71df5ecdbcca
xundaoxd <xundaoxd@outlook.com>
2022-09-26 21:02:31 +0800

    support MasonInstallAll

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,3 +2,8 @@ require('mason').setup({
     install_root_dir = vim.fn.stdpath('config')..'/pack/mason'
 })
 
+local packages = {'clangd', 'cmake-language-server', 'pyright', 'bash-language-server'}
+
+local mason_cmd = require('mason.api.command')
+vim.api.nvim_create_user_command("MasonInstallAll", function() mason_cmd.MasonInstall(packages) end, {})
+
