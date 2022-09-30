0f81d1cbbeede420f7084ca2e29b6889bc5555e3
xundaoxd <xundaoxd@outlook.com>
2022-09-30 22:58:07 +0800

    add awk_ls

--- nvim/lua/plugins/common.lua
+++ nvim/lua/plugins/common.lua
@@ -2,7 +2,8 @@ local lsp2server = {
     clangd = 'clangd',
     cmake = 'cmake-language-server',
     pyright = 'pyright',
-    bashls = 'bash-language-server'
+    bashls = 'bash-language-server',
+    awk_ls = 'awk-language-server',
 }
 
 return {
