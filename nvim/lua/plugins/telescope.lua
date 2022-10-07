d10e115a1b635d4f49c0e6e2431cc76efd77a7c0
xundaoxd <xundaoxd@outlook.com>
2022-10-07 19:50:51 +0800

    strip telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 require('telescope').setup()
 
-vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end)
-vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end)
-vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end)
-vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files() end)
+vim.keymap.set('n', '<leader>fg', function() builtin.live_grep() end)
 
