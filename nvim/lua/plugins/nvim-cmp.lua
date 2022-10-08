f22b0b22548f22a644364dcda36345bea258103b
yinchaogao <yinchaogao@deeproute.ai>
2022-10-08 10:59:09 +0800

    nvim-cmp improve

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,106 +1,62 @@
 require('luasnip/loaders/from_vscode').lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 
-local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
-}
-
+-- 'i' = insert mode
+-- 'c' = command mode
+-- 's' = select mode
 local cmp = require('cmp')
 cmp.setup({
     snippet = {
         expand = function(args) require('luasnip').lsp_expand(args.body) end
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-        ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping({
-            i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-            c = function(fallback)
-                if cmp.visible() then
-                    local e = cmp.get_active_entry()
-                    if e == nil or vim.tbl_isempty(e) then
-                        cmp.close()
-                    else
-                        cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-                    end
-                else
-                    fallback()
-                end
-            end
-        }),
-    }),
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered()
     },
+    mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-j>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                local e = cmp.get_selected_entry()
+                if e == nil or vim.tbl_isempty(e) then
+                    cmp.close()
+                else
+                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                end
+            else
+                fallback()
+            end
+            end),
+    }),
     formatting = {
         fields = { "kind", "abbr", "menu" },
-        format = function(entry, item)
-            item.kind = string.format("%s", kind_icons[item.kind])
-            item.menu = ({
-                nvim_lsp = '[LSP]',
-                luasnip = '[Snippet]',
-                buffer = '[Buffer]',
-                path = '[Path]',
-                calc = '[Calc]',
-                emoji = '[Emoji]',
-            })[entry.source.name]
-            return item
-        end,
+        format = require('lspkind').cmp_format({
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+            before = function(entry, vim_item)
+                vim_item.menu = '['..string.upper(entry.source.name)..']'
+                return vim_item
+            end
+        })
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
         { name = 'buffer' },
+        { name = 'luasnip' },
         { name = 'path' },
         { name = 'calc' },
         { name = 'emoji' },
+        { name = 'nvim_lsp' },
     }),
 })
 
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-        { name = 'path' },
         { name = 'cmdline' }
     })
 })
