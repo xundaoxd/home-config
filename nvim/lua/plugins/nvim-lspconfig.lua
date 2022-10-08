2f4f4917bc5608ceaf911acd0deab4984e82f2f8
yinchaogao <yinchaogao@deeproute.ai>
2022-10-08 11:18:40 +0800

    strip nvim lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,3 @@
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        virtual_text = {
-            space = 4,
-            prefix = ''
-        }
-    }
-)
-
 local on_attach = function(client, bufnr)
     require('lsp_signature').on_attach({
         bind = true,
@@ -17,13 +7,15 @@ local on_attach = function(client, bufnr)
     }, bufnr)
     require('lsp-format').on_attach(client)
 
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
     local bufopts = { remap=false, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.definition, bufopts)
+
     vim.keymap.set('n', 'd[', vim.diagnostic.goto_prev, bufopts)
     vim.keymap.set('n', 'd]', vim.diagnostic.goto_next, bufopts)
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<leader>fm', vim.lsp.buf.formatting, bufopts)
 end
 
