216e078ff934c89196e6dec0611179fce6c9d7d9
xundaoxd <xundaoxd@outlook.com>
2022-10-11 10:54:38 +0800

    add shortcut for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>ft', builtin.tags)
 vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols)
 
+vim.keymap.set('n', '<leader>fi', builtin.lsp_incoming_calls)
+vim.keymap.set('n', '<leader>fo', builtin.lsp_outgoing_calls)
+
