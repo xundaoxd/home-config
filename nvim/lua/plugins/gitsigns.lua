b50adb33c3a081696025298f05454b0730e183df
xundaoxd <xundaoxd@outlook.com>
2022-10-12 17:27:43 +0800

    add gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -6,3 +6,8 @@ vim.keymap.set({ 'n', 'v' }, '<leader>hh', gs.preview_hunk, opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>hk', gs.prev_hunk, opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>hj', gs.next_hunk, opts)
 
+vim.keymap.set({ 'n', 'v' }, '<leader>hs', gs.stage_buffer, opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>hu', gs.undo_stage_hunk, opts)
+
+vim.keymap.set({ 'n', 'v' }, '<leader>hr', gs.reset_buffer, opts)
+
