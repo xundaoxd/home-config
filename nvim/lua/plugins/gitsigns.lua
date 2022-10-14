f3074220109e6e07bc0f05014e02e981700e19bf
xundaoxd <xundaoxd@outlook.com>
2022-10-14 10:55:41 +0800

    improve gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -6,8 +6,11 @@ vim.keymap.set({ 'n', 'v' }, '<leader>hh', gs.preview_hunk, opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>hk', gs.prev_hunk, opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>hj', gs.next_hunk, opts)
 
-vim.keymap.set({ 'n', 'v' }, '<leader>hs', gs.stage_buffer, opts)
+
+vim.keymap.set({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, opts)
 vim.keymap.set({ 'n', 'v' }, '<leader>hu', gs.undo_stage_hunk, opts)
 
-vim.keymap.set({ 'n', 'v' }, '<leader>hr', gs.reset_buffer, opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>hR', gs.reset_buffer, opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>hS', gs.stage_buffer, opts)
 
