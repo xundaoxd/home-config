e855c4550f2f0dbe43d83aa70739a39fb382d31b
xundaoxd <xundaoxd@outlook.com>
2022-11-03 17:47:19 +0800

    update dwm autostart

--- dwm/autostart/autostart.sh
+++ dwm/autostart/autostart.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 feh --bg-fill --randomize ~/Pictures/Wallpaper/* &
-
 picom &
+
+export IDENTIFIER="unicode"
+export SEP1="["
+export SEP2="]"
+
+dwm_date() {
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+    else
+        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+while true; do
+    upperbar=""
+    upperbar="$upperbar$(dwm_date)"
+
+    xsetroot -name "$upperbar"
+    sleep 1
+done &
 
