530c7f03df462d0a68ba22f45dba27b6c96a22d0
xundaoxd <xundaoxd@outlook.com>
2022-11-05 07:21:35 +0800

    create soft link for nvim, ranger

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,3 +5,6 @@ self_dir=`realpath $(dirname $0)`
 mkdir -p ~/.config
 ln -sf $self_dir ~/.config/
 
+mkdir -p ~/.local/bin
+[ ! -e ~/.local/bin/vim ] && ln -s `which nvim` ~/.local/bin/vim
+
--- ranger/install.sh
+++ ranger/install.sh
@@ -5,3 +5,6 @@ self_dir=`realpath $(dirname $0)`
 mkdir -p ~/.config
 ln -sf $self_dir ~/.config/ranger
 
+mkdir -p ~/.local/bin
+[ ! -e ~/.local/bin/ra ] && ln -s `which ranger` ~/.local/bin/ra
+
