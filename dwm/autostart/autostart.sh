abb0963539bbe80b76fcc9cb136233069ed6d915
xundaoxd <xundaoxd@outlook.com>
2022-11-06 19:09:03 +0800

    add battery label in dwm

--- dwm/autostart/autostart.sh
+++ dwm/autostart/autostart.sh
@@ -3,23 +3,28 @@
 feh --bg-fill --randomize ~/Pictures/Wallpaper/* &
 picom &
 
-export IDENTIFIER="unicode"
 export SEP1="["
 export SEP2="]"
 
 dwm_date() {
     printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
-    else
-        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
-    fi
+    printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+    printf "%s\n" "$SEP2"
+}
+
+dwm_battery() {
+    # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
+    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT0/status)
+    printf "%s" "$SEP1"
+    printf "🔋 %s%% %s" "$CHARGE" "$STATUS"
     printf "%s\n" "$SEP2"
 }
 
 while true; do
     upperbar=""
     upperbar="$upperbar$(dwm_date)"
+    upperbar="$upperbar$(dwm_battery)"
 
     xsetroot -name "$upperbar"
     sleep 1
