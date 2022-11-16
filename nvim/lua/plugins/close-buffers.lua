7ae1d609a2d24a16f77c7443926cc20e26807b53
xundaoxd <xundaoxd@outlook.com>
2022-11-16 12:47:02 +0800

    fresh bufferline using switch_buf

--- nvim/lua/plugins/close-buffers.lua
+++ nvim/lua/plugins/close-buffers.lua
@@ -1,5 +1,10 @@
+local utils = require('utils')
+
 require('close_buffers').setup()
 
-vim.keymap.set('n', '<leader>th', function() require('close_buffers').delete({type = 'hidden'}) end, { silent = true, remap = false })
+vim.keymap.set('n', '<leader>th', function()
+    require('close_buffers').delete({type = 'hidden'})
+    utils.switch_buf(0)
+end, { silent = true, remap = false })
 vim.keymap.set('n', '<leader>tc', function() require('close_buffers').delete({type = 'this'}) end, { silent = true, remap = false })
 
