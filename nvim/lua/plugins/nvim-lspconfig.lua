d129780054a85d0ca349efe397cdb32d014c19a8
xundaoxd <xundaoxd@outlook.com>
2022-11-19 13:19:54 +0800

    search for references in lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,7 @@ local on_attach = function(client, bufnr)
     local bufopts = { remap=false, silent=true, buffer=bufnr }
     vim.keymap.set('n', '<leader>gd', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', '<leader>gD', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
 
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
