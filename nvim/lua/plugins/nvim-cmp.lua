3dea30f44b9e1dc4d992798f35cd55252375d7b5
xundaoxd <xundaoxd@outlook.com>
2022-12-09 21:04:03 +0800

    update lspkind

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -35,13 +35,9 @@ cmp.setup({
     formatting = {
         fields = { "kind", "abbr", "menu" },
         format = require('lspkind').cmp_format({
-            mode = 'symbol',
+            mode = 'symbol_text',
             maxwidth = 50,
             ellipsis_char = '...',
-            before = function(entry, vim_item)
-                vim_item.menu = '['..string.upper(entry.source.name)..']'
-                return vim_item
-            end
         })
     },
     sources = cmp.config.sources({
