b75cc708a90c150630a3521c57bbfb54b17e2caf
xundaoxd <xundaoxd@outlook.com>
2022-12-21 14:55:18 +0800

    update sp keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,7 +9,7 @@ vim.tbl_map(function(i)
     vim.keymap.set('n', '<leader>w'..i, '<C-w>'..i, opt)
 end, {'h', 'j', 'k', 'l'})
 
-vim.keymap.set('n', '<leader>ps', ':split<CR>', opt)
+vim.keymap.set('n', '<leader>sp', ':split<CR>', opt)
 vim.keymap.set('n', '<leader>vs', ':vs<CR>', opt)
 
 vim.keymap.set('i', '<C-h>', '<left>', opt)
