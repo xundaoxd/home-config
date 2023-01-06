a9e6a6e0e7df90edce001f302f355025207b68aa
xundaoxd <xundaoxd@outlook.com>
2023-01-06 22:26:20 +0800

    format mason.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,7 +2,12 @@ require('mason').setup({
     install_root_dir = vim.fn.stdpath('config')..'/pack/mason'
 })
 
-local packages = {'clangd', 'cmake-language-server', 'pyright', 'bash-language-server', 'shellcheck', 'awk-language-server', 'opencl-language-server', 'dockerfile-language-server', 'marksman', 'typescript-language-server'}
+local packages = {
+    'clangd', 'cmake-language-server', 'pyright',
+    'bash-language-server', 'shellcheck', 'awk-language-server',
+    'opencl-language-server', 'dockerfile-language-server',
+    'marksman', 'typescript-language-server'
+}
 local mason_cmd = require('mason.api.command')
 vim.api.nvim_create_user_command("MasonInstallAll", function() mason_cmd.MasonInstall(packages) end, {})
 
