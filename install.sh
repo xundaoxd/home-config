cc1345c89c83712a7b979cb8e8f589273f3ee903
yinchaogao <yinchaogao@deeproute.ai>
2023-03-09 11:08:15 +0800

    rewrite install script

--- install.sh
+++ install.sh
@@ -3,6 +3,59 @@
 self_path=`realpath ${BASH_SOURCE[0]:-$0}`
 self_dir=`dirname $self_path`
 
-./zsh/install.sh
-./nvim/install.sh
+die() {
+    echo "$0"
+    exit 255
+}
+
+init() {
+    cat >> ~/.xprofile << \EOF
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+EOF
+}
+
+install_osh() {
+    [ -e ~/.oh-my-zsh ] && die "oh my zsh installed, exited"
+
+    git clone https://gitee.com/mirrors/oh-my-zsh.git ~/.oh-my-zsh
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+    cat >> ~/.zshrc << \EOF
+add_local() {
+    [ -d $1/bin ] && export PATH="$1/bin:$PATH"
+}
+add_local $HOME/.local
+EOF
+}
+
+install_nvim() {
+    [ -e ~/.config/nvim ] && die "nvim installed, exited"
+
+    mkdir -p ~/.config
+    ln -sf "$self_dir/nvim" ~/.config/
+
+    mkdir -p ~/.local/bin
+    ln -sf "$(which nvim)" ~/.local/bin/vim
+
+    echo 'export EDITOR="nvim"' >> ~/.xprofile
+}
+
+install_ranger() {
+    mkdir -p ~/.config
+    ln -sf "$self_dir/ranger" ~/.config/
+
+    mkdir -p ~/.local/bin
+    [ ! -e ~/.local/bin/ra ] && ln -s "$(which ranger)" ~/.local/bin/ra
+}
+
+main(){
+    init
+    install_osh
+    install_nvim
+    install_ranger
+}
+
+main
 
