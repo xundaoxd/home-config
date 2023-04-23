2193c959d8a9a96fe996b1b8d61e16421bfdb99e
xundaoxd <xundaoxd@outlook.com>
2023-04-23 21:13:43 +0800

    delete custom keymap for nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,8 +5,6 @@ local function on_attach(bufnr)
     local api = require("nvim-tree.api")
     api.config.mappings.default_on_attach(bufnr)
 
-    vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-    vim.keymap.set('n', 'l', api.node.open.edit, opts('Open or Edit'))
 end
 
 require("nvim-tree").setup({
