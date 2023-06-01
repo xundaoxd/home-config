fe8c5186a4c9e88b7b8a04996b0aea5d1eabed4c
yinchaogao <yinchaogao@deeproute.ai>
2023-06-01 19:04:34 +0800

    update lsp keymap

--- nvim/lua/plugins/nvim-cmp/lsp.lua
+++ nvim/lua/plugins/nvim-cmp/lsp.lua
@@ -6,19 +6,19 @@ local lsp_setup = function(lsp, opts)
         capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         on_attach = function(client, bufnr)
             local opts = { buffer = bufnr }
-            vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', '<leader>gD', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
 
-            vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 
-            vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+            vim.keymap.set('n', '<leader>gl', vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', '<leader>[d', vim.diagnostic.goto_prev, opts)
+            vim.keymap.set('n', '<leader>]d', vim.diagnostic.goto_next, opts)
 
-            vim.keymap.set('n', 'fm', function() vim.lsp.buf.format({async = true}) end, opts)
+            vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format({async = true}) end, opts)
 
             require('lsp_signature').on_attach({
                 bind = true,
