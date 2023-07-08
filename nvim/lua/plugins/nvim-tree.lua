20641b2dbf0778355d317199fc0fd873dc44ab2a
xundaoxd <xundaoxd@outlook.com>
2023-07-08 19:18:51 +0800

    update nvim-tree keymap

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,11 +3,30 @@ local function on_attach(bufnr)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
     local api = require("nvim-tree.api")
-    api.config.mappings.default_on_attach(bufnr)
 
-    vim.keymap.del('n', '<BS>', { buffer = bufnr })
     vim.keymap.set('n', 'h',  api.node.navigate.parent_close, opts('Close Directory'))
     vim.keymap.set('n', 'l',  api.node.open.edit, opts('Open'))
+    vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close, opts('Close Directory'))
+    vim.keymap.set('n', '<CR>',  api.node.open.edit, opts('Open'))
+
+    vim.keymap.set('n', 'L',  api.tree.expand_all, opts('Expand All'))
+    vim.keymap.set('n', 'H',  api.tree.collapse_all, opts('Collapse All'))
+    vim.keymap.set('n', 'R',  api.tree.reload, opts('Refresh'))
+    vim.keymap.set('n', 'i',  api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+    vim.keymap.set('n', 'f',  api.live_filter.start, opts('Filter'))
+    vim.keymap.set('n', 'F',  api.live_filter.clear, opts('Clean Filter'))
+    vim.keymap.set('n', 's',  api.tree.search_node, opts('Search'))
+
+    vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+    vim.keymap.set('n', 'yy', api.fs.copy.node, opts('Copy'))
+    vim.keymap.set('n', 'dd', api.fs.cut, opts('Cut'))
+    vim.keymap.set('n', 'pp', api.fs.paste, opts('Paste'))
+    vim.keymap.set('n', 'de', api.fs.remove, opts('Delete'))
+    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+
+    vim.keymap.set('n', 'yp', api.fs.copy.absolute_path, opts('Copy Absolute path'))
+    vim.keymap.set('n', 'yr', api.fs.copy.relative_path, opts('Copy Relative path'))
+    vim.keymap.set('n', 'yn', api.fs.copy.filename, opts('Copy filename'))
 end
 
 require("nvim-tree").setup({
