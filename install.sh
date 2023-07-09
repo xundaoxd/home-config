f13d1ee4b8ff59bb06776cd85e527f12d1121682
xundaoxd <xundaoxd@outlook.com>
2023-07-09 15:20:57 +0800

    update install

--- install.sh
+++ install.sh
@@ -4,65 +4,76 @@ set -e
 self_path=$(realpath "${BASH_SOURCE[0]:-$0}")
 self_dir=$(dirname "$self_path")
 
+opt_force=n
 opt_group='all'
 
 die() {
-    echo "$0"
+    echo "$@"
     exit 255
 }
 
 init() {
-    cat > ~/.zprofile << \EOF
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-
-[[ -d $HOME/.local/bin ]] && export PATH="$HOME/.local/bin:$PATH"
-EOF
+    ln -sf -T "$self_dir/assets/profile" ~/.bash_profile
+    cp "$self_dir/assets/bashrc" ~/.bashrc
 }
 
 install_osh() {
-    [ -e ~/.oh-my-zsh ] && die "oh my zsh installed, exit"
+    [[ $opt_force == y ]] && rm -rf ~/.oh-my-zsh ~/.zprofile ~/.zshrc
+    [[ -e ~/.oh-my-zsh ]] && return
 
     git clone https://gitee.com/mirrors/oh-my-zsh.git ~/.oh-my-zsh
     cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+    echo '.     ~/.bash_profile' >> ~/.zprofile
+    echo '.     ~/.bashrc' >> ~/.zshrc
 }
 
 install_nvim() {
-    [[ -e ~/.local/bin/vim ]] && die "nvim installed, exit"
+    [[ -e ~/.local/bin/vim ]] && return
 
     mkdir -p ~/.config
-    ln -sf "$self_dir/nvim" ~/.config/
-
+    ln -sf -T "$self_dir/nvim" ~/.config/nvim
     mkdir -p ~/.local/bin
-    ln -sf "$(which nvim)" ~/.local/bin/vim
-
-    echo 'export EDITOR="nvim"' >> ~/.zshrc
+    ln -sf -T "$(which nvim)" ~/.local/bin/vim
 }
 
 install_ranger() {
-    [[ -e ~/.local/bin/ra ]] && die "ranger installed, exit"
-    mkdir -p ~/.config
-    ln -sf "$self_dir"/ranger ~/.config/
+    [[ -e ~/.local/bin/ra ]] && return
 
+    mkdir -p ~/.config
+    ln -sf -T "$self_dir"/ranger ~/.config/ranger
     mkdir -p ~/.local/bin
-    ln -s "$(which ranger)" ~/.local/bin/ra
+    ln -sf -T "$(which ranger)" ~/.local/bin/ra
 }
 
 install_bspwm() {
-    cp -r "$self_dir"/bspwm/* ~/.config/
+    for f in $(ls -A "$self_dir/bspwm"); do
+        [[ $opt_force == y ]] && rm -rf "$HOME/.config/$f"
+        ln -sf -T "$self_dir/bspwm/$f" "$HOME/.config/$f" || true
+    done
 }
 
 install_obsidian() {
-    [[ -e ~/.local/bin/obsidian ]] && die "obsidian installed, exit"
+    [[ $opt_force == y ]] && rm -rf ~/.local/bin/obsidian{,.config}
+    [[ -e ~/.local/bin/obsidian ]] && return
+
     wget -O ~/.local/bin/obsidian https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.5/Obsidian-1.3.5.AppImage
     chmod +x ~/.local/bin/obsidian
     mkdir -p ~/.local/bin/obsidian.config
 }
 
+while getopts 'f' name; do
+    case $name in
+        f)  opt_force=y;;
+        ?)  die "undefine flag: $name";
+    esac
+done
+shift $((OPTIND - 1))
+
 (( $# > 0 )) && opt_group="$*"
 [[ $opt_group == "all" ]] && opt_group="osh nvim ranger bspwm obsidian"
 
+init
 for t in $opt_group; do
     install_$t
 done
