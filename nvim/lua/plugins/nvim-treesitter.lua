bce7bae6457b8f3e21026d94c90476bb0d3d7495
yinchaogao <yinchaogao@deeproute.ai>
2023-07-11 14:06:57 +0800

    update nvim-treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,19 @@
 require('nvim-treesitter.configs').setup({
     ensure_installed = {
         'cmake', 'make', 'ninja',
-        'c', 'cpp', 'cuda', 'markdown', 'python', 'lua', 'bash', 'dot', 'javascript',
+        'c', 'cpp', 'cuda', 'markdown', 'python', 'lua', 'bash', 'dot', 'javascript', 'java',
         'llvm', 'mlir', 'vim', 'dockerfile', 'gitignore', 'json'
     },
     highlight = {
         enable = true,
+        disable = function(lang, buf)
+            local max_filesize = 1024 * 1024
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+        additional_vim_regex_highlighting = false,
     },
 })
 
