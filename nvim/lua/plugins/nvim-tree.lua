c5e8b11fb48fce7bde8373060cd04ae5150c0786
yinchaogao <yinchaogao@deeproute.ai>
2023-09-18 10:37:06 +0800

    update nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,29 +4,30 @@ local function on_attach(bufnr)
     end
     local api = require("nvim-tree.api")
 
-    vim.keymap.set('n', 'h',  api.node.navigate.parent_close, opts('Close Directory'))
-    vim.keymap.set('n', 'l',  api.node.open.edit, opts('Open'))
-    vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close, opts('Close Directory'))
-    vim.keymap.set('n', '<CR>',  api.node.open.edit, opts('Open'))
+    vim.keymap.set('n', 'h',     api.node.navigate.parent_close,    opts('Close Directory'))
+    vim.keymap.set('n', 'l',     api.node.open.edit,                opts('Open'))
+    vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close,    opts('Close Directory'))
+    vim.keymap.set('n', '<CR>',  api.node.open.edit,                opts('Open'))
 
-    vim.keymap.set('n', 'L',  api.tree.expand_all, opts('Expand All'))
-    vim.keymap.set('n', 'H',  api.tree.collapse_all, opts('Collapse All'))
-    vim.keymap.set('n', 'R',  api.tree.reload, opts('Refresh'))
-    vim.keymap.set('n', 'i',  api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
-    vim.keymap.set('n', 'f',  api.live_filter.start, opts('Filter'))
-    vim.keymap.set('n', 'F',  api.live_filter.clear, opts('Clean Filter'))
-    vim.keymap.set('n', 's',  api.tree.search_node, opts('Search'))
+    vim.keymap.set('n', 'L',     api.tree.expand_all,               opts('Expand All'))
+    vim.keymap.set('n', 'H',     api.tree.collapse_all,             opts('Collapse All'))
+    vim.keymap.set('n', 'R',     api.tree.reload,                   opts('Refresh'))
+    vim.keymap.set('n', 'i',     api.tree.toggle_gitignore_filter,  opts('Toggle Git Ignore'))
+    vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node,      opts('CD'))
+    vim.keymap.set('n', '<C-[>', api.tree.change_root_to_parent,    opts('UP'))
+    vim.keymap.set('n', 's',     api.tree.search_node,              opts('Search'))
+    vim.keymap.set('n', 'f',     api.live_filter.start,             opts('Filter'))
+    vim.keymap.set('n', 'F',     api.live_filter.clear,             opts('Clean Filter'))
 
-    vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
-    vim.keymap.set('n', 'yy', api.fs.copy.node, opts('Copy'))
-    vim.keymap.set('n', 'dd', api.fs.cut, opts('Cut'))
-    vim.keymap.set('n', 'pp', api.fs.paste, opts('Paste'))
-    vim.keymap.set('n', 'de', api.fs.remove, opts('Delete'))
-    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
-
-    vim.keymap.set('n', 'yp', api.fs.copy.absolute_path, opts('Copy Absolute path'))
-    vim.keymap.set('n', 'yr', api.fs.copy.relative_path, opts('Copy Relative path'))
-    vim.keymap.set('n', 'yn', api.fs.copy.filename, opts('Copy filename'))
+    vim.keymap.set('n', 'a',     api.fs.create,                     opts('Create'))
+    vim.keymap.set('n', 'yy',    api.fs.copy.node,                  opts('Copy'))
+    vim.keymap.set('n', 'dd',    api.fs.cut,                        opts('Cut'))
+    vim.keymap.set('n', 'pp',    api.fs.paste,                      opts('Paste'))
+    vim.keymap.set('n', 'de',    api.fs.remove,                     opts('Delete'))
+    vim.keymap.set('n', 'r',     api.fs.rename,                     opts('Rename'))
+    vim.keymap.set('n', 'yp',    api.fs.copy.absolute_path,         opts('Copy Absolute path'))
+    vim.keymap.set('n', 'yr',    api.fs.copy.relative_path,         opts('Copy Relative path'))
+    vim.keymap.set('n', 'yn',    api.fs.copy.filename,              opts('Copy filename'))
 end
 
 require("nvim-tree").setup({
@@ -44,5 +45,10 @@ require("nvim-tree").setup({
     },
     on_attach = on_attach,
 })
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
 vim.keymap.set('n', '<leader>dr', '<cmd>NvimTreeToggle<CR>')
 
