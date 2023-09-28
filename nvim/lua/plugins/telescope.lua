5b288e4dff87a3280c55c827348f748aeda16b08
yinchaogao <yinchaogao@deeproute.ai>
2023-09-28 10:37:31 +0800

    update nvim tele

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -26,7 +26,6 @@ telescope.load_extension('fzf')
 
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('v', '<leader>fg', '<cmd>Telescope grep_string<CR>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope current_buffer_fuzzy_find<CR>')
 vim.keymap.set('n', '<leader>ft', '<cmd>Telescope tags<CR>')
 
