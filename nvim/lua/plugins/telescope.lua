e4bd1b313fb17490b41202156c58d93e0330ab61
yinchaogao <yinchaogao@deeproute.ai>
2023-10-26 15:13:13 +0800

    update keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -25,15 +25,15 @@ telescope.setup({
 })
 telescope.load_extension('fzf')
 
-vim.keymap.set('n', 'ff', builtin.find_files, {})
-vim.keymap.set('n', 'fg', builtin.live_grep, {})
-vim.keymap.set('n', 'fb', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', 'ft', builtin.tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>ft', builtin.tags, {})
 
-vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
-vim.keymap.set('n', 'gD', builtin.lsp_type_definitions, {})
-vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>gD', builtin.lsp_type_definitions, {})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
 
-vim.keymap.set('n', 'gs', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', 'gws', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>gs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>gws', builtin.lsp_workspace_symbols, {})
 
