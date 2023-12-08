800db49ca440aeecd4fe4a7d19ab5c900ccca9ee
yinchaogao <yinchaogao@deeproute.ai>
2023-12-08 14:56:10 +0800

    update format

--- homefs/.config/nvim/lua/plugins/telescope.lua
+++ homefs/.config/nvim/lua/plugins/telescope.lua
@@ -18,8 +18,8 @@ telescope.load_extension('fzf')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<leader>ft', builtin.tags, {})
 
+vim.keymap.set('n', '<leader>ft', builtin.tags, {})
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
 
