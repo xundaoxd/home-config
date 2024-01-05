cd3500eaf000667dfa568f85e171c3713d392c46
yinchaogao <yinchaogao@deeproute.ai>
2024-01-05 14:07:55 +0800

    update nvim

--- homefs/.config/nvim/lua/plugins/init.lua
+++ homefs/.config/nvim/lua/plugins/init.lua
@@ -3,14 +3,13 @@ local ensure_packer = function()
     local install_path = fn.stdpath('config')..'/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
         fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
         return true
     end
     return false
 end
 
 local packer_bootstrap = ensure_packer()
-
-vim.cmd('packadd packer.nvim')
 local packer = require('packer')
 packer.init({
     package_root = vim.fn.stdpath('config')..'/pack',
