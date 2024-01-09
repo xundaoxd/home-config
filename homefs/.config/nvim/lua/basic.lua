871815b70eac003fccec5477c93cfaec03b5423f
xundaoxd <xundaoxd@outlook.com>
2024-01-09 21:04:28 +0800

    update nvim

--- homefs/.config/nvim/lua/basic.lua
+++ homefs/.config/nvim/lua/basic.lua
@@ -10,11 +10,15 @@ vim.o.wrap = false
 vim.o.showcmd = true
 vim.o.scrolloff = 15
 
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
 vim.o.smarttab = true
 vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
 
 vim.o.mouse = 'a'
 vim.o.mousemoveevent = true
+vim.o.foldmethod = 'indent'
+vim.o.foldenable = false
+
+vim.o.filetype = true
 
