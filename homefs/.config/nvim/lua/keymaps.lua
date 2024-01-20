ca14e460ebba099352082d9877dcb1c44a55d59a
xundaoxd <xundaoxd@outlook.com>
2024-01-20 14:28:28 +0800

    update lsp keymaps

--- homefs/.config/nvim/lua/keymaps.lua
+++ homefs/.config/nvim/lua/keymaps.lua
@@ -50,14 +50,3 @@ vim.keymap.set({'n', 'v', 'i'}, '<down>', '<nop>', opts)
 vim.keymap.set({'n', 'v', 'i'}, '<left>', '<nop>', opts)
 vim.keymap.set({'n', 'v', 'i'}, '<right>', '<nop>', opts)
 
--- lsp
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
-vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-vim.keymap.set({'n', 'i'}, '<A-k>', vim.lsp.buf.signature_help)
-
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
-vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format)
-
--- homefs/.config/nvim/lua/plugins/telescope.lua
+++ homefs/.config/nvim/lua/plugins/telescope.lua
@@ -8,3 +8,7 @@ vim.keymap.set('n', 'fg', builtin.live_grep)
 vim.keymap.set('n', 'fb', builtin.current_buffer_fuzzy_find)
 vim.keymap.set('n', 'fs', builtin.lsp_workspace_symbols)
 
+-- lsp
+vim.keymap.set('n', 'gd', builtin.lsp_definitions)
+vim.keymap.set('n', 'gi', builtin.lsp_implementations)
+vim.keymap.set('n', 'gr', builtin.lsp_references)
