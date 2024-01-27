d6f24a19c3494b33aa02c0b154b7c032d1728ed2
xundaoxd <xundaoxd@outlook.com>
2024-01-27 20:57:30 +0800

    fix nvim

--- homefs/.config/nvim/lua/plugins/nvim-cmp/cmp.lua
+++ homefs/.config/nvim/lua/plugins/nvim-cmp/cmp.lua
@@ -4,6 +4,9 @@ require('luasnip.loaders.from_snipmate').lazy_load()
 
 local cmp = require('cmp')
 cmp.setup({
+    snippet = {
+        expand = function(args) require('luasnip').lsp_expand(args.body) end
+    },
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered()
