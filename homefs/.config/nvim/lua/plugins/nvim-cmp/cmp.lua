f115244e34dcbf9010e277e63c2cf7b984715a79
xundaoxd <xundaoxd@outlook.com>
2024-01-27 19:15:13 +0800

    update cmp

--- homefs/.config/nvim/lua/plugins/nvim-cmp/cmp.lua
+++ homefs/.config/nvim/lua/plugins/nvim-cmp/cmp.lua
@@ -1,18 +1,34 @@
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_snipmate").lazy_load()
+local luasnip = require('luasnip')
+require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_snipmate').lazy_load()
 
 local cmp = require('cmp')
 cmp.setup({
-    snippet = {
-        expand = function(args) require('luasnip').lsp_expand(args.body) end
-    },
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered()
     },
     mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.jumpable(1) then
+                luasnip.jump(1)
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
         ['<C-j>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(),
