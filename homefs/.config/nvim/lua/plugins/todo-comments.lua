22accbcf637d835040401b81fc2b8e38f58c747e
yinchaogao <yinchaogao@deeproute.ai>
2024-02-07 16:17:30 +0800

    show WARNING

--- homefs/.config/nvim/lua/plugins/todo-comments.lua
+++ homefs/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,4 +1,4 @@
 require('todo-comments').setup()
 
-vim.keymap.set('n', 'ft', '<cmd>TodoTelescope keywords=TODO,FIX,HACK<CR>')
+vim.keymap.set('n', 'ft', '<cmd>TodoTelescope keywords=TODO,FIX,HACK,WARNING<CR>')
 
