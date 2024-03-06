b9d0cd1a0ac3fdd225915e0b303375d47b19cc82
yinchaogao <yinchaogao@deeproute.ai>
2024-03-06 11:29:52 +0800

    update nvim

--- homefs/.config/nvim/lua/plugins/nvim-cmp/init.lua
+++ homefs/.config/nvim/lua/plugins/nvim-cmp/init.lua
@@ -1,3 +1,74 @@
 require('plugins.nvim-cmp.lsp')
-require('plugins.nvim-cmp.cmp')
+
+local luasnip = require('luasnip')
+require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_snipmate').lazy_load()
+
+local cmp = require('cmp')
+cmp.setup({
+    snippet = {
+        expand = function(args) require('luasnip').lsp_expand(args.body) end
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_next_item()
+        --     elseif luasnip.jumpable(1) then
+        --         luasnip.jump(1)
+        --     elseif luasnip.expandable() then
+        --         luasnip.expand()
+        --     else
+        --         fallback()
+        --     end
+        -- end, {'i', 's'}),
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_prev_item()
+        --     elseif luasnip.jumpable(-1) then
+        --         luasnip.jump(-1)
+        --     else
+        --         fallback()
+        --     end
+        -- end, {'i', 's'}),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-j>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm(),
+    }),
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = require('lspkind').cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+            ellipsis_char = '...',
+        })
+    },
+    sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'buffer' },
+        { name = 'emoji' },
+        { name = 'calc' },
+        { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+    }),
+})
+
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'buffer' }
+    })
+})
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'cmdline' }
+    })
+})
 
