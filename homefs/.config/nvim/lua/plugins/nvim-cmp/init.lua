cab8e5d231ea5fa9b330841e1fa38cb9c3f965db
xundaoxd <xundaoxd@outlook.com>
2024-03-22 21:32:09 +0800

    update cmp

--- homefs/.config/nvim/lua/plugins/nvim-cmp/init.lua
+++ homefs/.config/nvim/lua/plugins/nvim-cmp/init.lua
@@ -4,6 +4,12 @@ local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load()
 require('luasnip.loaders.from_snipmate').lazy_load()
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require('cmp')
 cmp.setup({
     snippet = {
@@ -14,29 +20,28 @@ cmp.setup({
         documentation = cmp.config.window.bordered()
     },
     mapping = cmp.mapping.preset.insert({
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_next_item()
-        --     elseif luasnip.jumpable(1) then
-        --         luasnip.jump(1)
-        --     elseif luasnip.expandable() then
-        --         luasnip.expand()
-        --     else
-        --         fallback()
-        --     end
-        -- end, {'i', 's'}),
-        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_prev_item()
-        --     elseif luasnip.jumpable(-1) then
-        --         luasnip.jump(-1)
-        --     else
-        --         fallback()
-        --     end
-        -- end, {'i', 's'}),
-        -- ['<esc>'] = cmp.mapping.abort(),
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        -- ['<Tab>'] = cmp.mapping.select_next_item(),
+        -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         ['<A-k>'] = cmp.mapping.scroll_docs(-4),
         ['<A-j>'] = cmp.mapping.scroll_docs(4),
         ['<CR>'] = cmp.mapping.confirm(),
