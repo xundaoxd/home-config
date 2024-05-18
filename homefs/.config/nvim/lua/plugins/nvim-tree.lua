bb05a68db0272c59eb6fe3ef9077c3fdd9953fc5
xundaoxd <xundaoxd@outlook.com>
2024-05-18 11:48:12 +0800

    update ranger, neovim

--- homefs/.config/nvim/lua/plugins/nvim-tree.lua
+++ homefs/.config/nvim/lua/plugins/nvim-tree.lua
@@ -9,26 +9,25 @@ local function on_attach(bufnr)
 
     vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
     vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-
     vim.keymap.set('n', 'L', api.tree.expand_all, opts('Expand All'))
     vim.keymap.set('n', 'H', api.tree.collapse_all, opts('Collapse All'))
 
-    vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
-    vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
-    vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+    vim.keymap.set('n', ']', api.tree.change_root_to_node, opts('CD'))
+    vim.keymap.set('n', '[', api.tree.change_root_to_parent, opts('Up'))
 
+    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
     vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
     vim.keymap.set('n', 'yy', api.fs.copy.node, opts('Copy'))
     vim.keymap.set('n', 'dd', api.fs.cut, opts('Cut'))
     vim.keymap.set('n', 'pp', api.fs.paste, opts('Paste'))
     vim.keymap.set('n', 'de', api.fs.remove, opts('Delete'))
-    vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
     vim.keymap.set('n', 'yp', api.fs.copy.absolute_path, opts('Copy Absolute path'))
     vim.keymap.set('n', 'yr', api.fs.copy.relative_path, opts('Copy Relative path'))
     vim.keymap.set('n', 'yn', api.fs.copy.filename, opts('Copy filename'))
 
-    vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
-    vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
+    vim.keymap.set('n', 'i', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+    vim.keymap.set('n', '/', api.tree.search_node, opts('Search'))
+    vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
 end
 
 require("nvim-tree").setup({
