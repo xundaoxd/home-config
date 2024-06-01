d2a97daf303e03d2679d790d3c290d0b749138fa
xundaoxd <xundaoxd@outlook.com>
2024-06-01 20:00:11 +0800

    fix bash

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ init() {
     [[ -e ~/.zshrc ]] && return
 
     cp $self_dir/homefs/.xprofile ~/.xprofile
-    cp $self_dir/homefs/Pictures ~/
+    cp -r $self_dir/homefs/Pictures ~/
 
     cp $self_dir/homefs/.zshrc ~/.zshrc
     cp $self_dir/homefs/.zprofile ~/.zprofile
