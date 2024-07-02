525b754bae9aeb65c4e0267e6b2e341ce7ae0de2
yinchaogao <yc_X@outlook.com>
2024-07-02 16:52:20 +0800

    update nvim

--- homefs/.config/nvim/lua/plugins/diffview.lua
+++ homefs/.config/nvim/lua/plugins/diffview.lua
@@ -1,20 +1,19 @@
 return {
     'sindrets/diffview.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        view = {
-            default = {
-                layout = 'diff2_horizontal',
-            },
-            merge_tool = {
-                disable_diagnostics = true,
+    config = function()
+        require('diffview').setup({
+            view = {
+                default = {
+                    layout = 'diff2_horizontal',
+                },
+                merge_tool = {
+                    disable_diagnostics = true,
+                }
             }
-        }
-    },
-    lazy = true,
-    keys = {
-        { '<leader>do', '<cmd>DiffviewOpen<cr>' },
-        { '<leader>dc', '<cmd>DiffviewClose<cr>' },
-        { '<leader>dh', '<cmd>DiffviewFileHistory<cr>' },
-    },
+        })
+        vim.keymap.set('n', '<leader>do', '<cmd>DiffviewOpen<cr>')
+        vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<cr>')
+        vim.keymap.set('n', '<leader>dh', '<cmd>DiffviewFileHistory<cr>')
+    end,
 }
