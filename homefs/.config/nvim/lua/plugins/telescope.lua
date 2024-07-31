c4d574ac726c93e3f8e8cb8a62f64c37e22414e7
yinchaogao <yc_X@outlook.com>
2024-07-31 09:57:12 +0800

    update nvim

--- homefs/.config/nvim/lua/plugins/telescope.lua
+++ homefs/.config/nvim/lua/plugins/telescope.lua
@@ -42,7 +42,6 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files)
         vim.keymap.set('n', '<leader>fg', builtin.live_grep)
         vim.keymap.set('n', '<leader>fb', builtin.buffers)
-        vim.keymap.set('n', '<leader>fs', builtin.help_tags)
 
         vim.keymap.set('n', '<leader>gr', builtin.lsp_references)
         vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions)
